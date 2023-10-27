fd60a545e9beef14de964d8a825d917f499e3865
Łukasz <lukaszkogut1996@gmail.com>
2023-10-27 21:43:59 +0200

    update task-01

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesList = document.querySelector('#categories')
+const categoriesItems = document.querySelectorAll('.item')
+console.log(`Number of categories: ${categoriesItems.length}`)
+
+categoriesItems.forEach(category => {
+    const categoryName = category.querySelector('h2').textContent
+    const elementsCount = category.querySelectorAll('li').length
+
+    console.log(`Category: ${categoryName} Elements: ${elementsCount}`);
+})
