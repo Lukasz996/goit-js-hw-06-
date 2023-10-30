2fa96594f72aba2e29dd55695b0f047c03e64548
Łukasz <lukaszkogut1996@gmail.com>
2023-10-30 21:08:19 +0100

    task2 and 3

--- js/task-02.js
+++ js/task-02.js
@@ -1,14 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
 
 
-const liElements = ingrediens.map((ingredient) => {
-  const li = document.createElement('li')
-  
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+
+const liElements = ingredients.map(ingredient => {
+	const li = document.createElement('li')
+	li.textContent = ingredient
+	li.classList.add('item')
+	return li
 })
+
+document.querySelector("#ingredients").append(...liElements)
