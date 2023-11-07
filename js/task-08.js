efee3cd281d3d926be2c3e8898730feaef045d7e
Łukasz <lukaszkogut1996@gmail.com>
2023-11-07 21:19:57 +0100

    task 7-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const submit = document.querySelector('.login-form')
 
+submit.addEventListener('submit', event => {
+	event.preventDefault()
+
+	const email = submit.elements.email
+	const password = submit.elements.password
+
+	if (email.value === '' || password.value === '') {
+		alert('wypełnij wszystkie pola formularza')
+		return
+	} else {
+		const formLogin = {
+			email: email.value,
+			password: password.value,
+		}
+		console.log(formLogin)
+
+		submit.reset()
+	}
+})
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
+const changeColorBtn = document.querySelector('.change-color')
+const changeSpan = document.querySelector('.color')
+
+changeColorBtn.addEventListener('click', () => {
+	const newColor = getRandomHexColor()
+	document.body.style.backgroundColor = newColor
+	changeSpan.textContent = newColor
+})
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`
 }
